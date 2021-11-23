d8561747f63e9073f70e6020a5dfa09cc1df1287
AppleKH <90026034+AppleKH@users.noreply.github.com>
2021-11-23 17:42:10 +0300

    Протоколы шифрования - Алиева Х. ПИ20-1

---
+++ протоколы шифрования (доп.зад.)/dop1.py
@@ -0,0 +1,17 @@
+def vigener_vernam_cipher(string, k):
+
+    lb, rb = 0, 1114111
+    res = ''
+    for ind, s in enumerate(string):
+        cur_k = ord(k[ind % len(k)])
+        res += chr(ord(s) ^ cur_k)
+    return res
+
+
+
+s = 'One two three four five six seven eight nine ten. And some other words to gain string size!'
+new_s = vigener_vernam_cipher(s, 'qwe')
+old_s = vigener_vernam_cipher(new_s, 'qwe')
+print(f's = {s}\n'
+      f'new_s = {new_s}\n'
+      f'old_s = {old_s}')
